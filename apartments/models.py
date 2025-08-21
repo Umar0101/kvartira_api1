4dd4e66ddd63c616b670b466ad94c50f84e3fdd5
Umar0101 <umartaalaybek@gmail.com>
2025-08-21 20:50:43 +0600

    Serializers1

--- apartments/models.py
+++ apartments/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.contrib.auth import get_user_model
 from phonenumber_field.modelfields import PhoneNumberField
-
+from django.core.validators import MinValueValidator, MaxValueValidator
 # Create your models here.
 
 
@@ -10,6 +10,9 @@ User = get_user_model()
 
 class Amenity(models.Model):
     name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
 
 
 class Apartment(models.Model):
@@ -20,12 +23,40 @@ class Apartment(models.Model):
     bedroom = models.IntegerField(blank=True, null=True)
     bed = models.IntegerField(blank=True, null=True)
     bathroom = models.IntegerField(blank=True, null=True)
-    amenities = models.ManyToManyField(Amenity, blank=True, many=True)
+    amenities = models.ManyToManyField(Amenity, blank=True)
+    cover_photo = models.ImageField(upload_to='apartment/cover_photos',blank=True, null=True)
     price = models.DecimalField(max_digits=10, decimal_places= 2)
-    phone_number = PhoneNumberField(region='KG')    
+    phone_number = PhoneNumberField(region='KG')
+    average_rating = models.FloatField(default=0)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def update_rating(self):
+        ratings = self.ratings.all()
+        if ratings.exists():
+            self.average_rating = sum(r.value for r in ratings) / ratings.count()
+        else:
+            self.average_rating = 0 
+        self.save(update_fields=['average_rating'])
+
+    def __str__(self):
+        return f'{self.title} - {self.user}'
 
 class ApartmentPhoto(models.Model):
     apartment = models.ForeignKey(Apartment, on_delete=models.CASCADE, related_name='apartmentphotos')
     photo = models.ImageField(upload_to= 'apartment/photos/', blank=True, null=True)
 
+    def __str__(self):
+        return f'Фото - {self.apartment.title}'
+    
+class Rating(models.Model):
+    user = models.ForeignKey(User, on_delete=models.PROTECT, related_name='ratings')
+    apartment = models.ForeignKey(Apartment, on_delete=models.CASCADE, related_name='ratings')
+    value = models.PositiveSmallIntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
 
+    class Meta:
+        unique_together = ('user', 'apartment')
+
+    def __str__(self):
+        return f'{self.user} - {self.apartment} - {self.value}'
+    
--- apartments/serializers.py
+++ apartments/serializers.py
@@ -1,11 +1,56 @@
 from rest_framework.serializers import ModelSerializer
 
-from .models import Amenity, Apartment, ApartmentPhoto
+from .models import Amenity, Apartment, ApartmentPhoto, Rating
+
+class ApartmentPhotoSerializer(ModelSerializer):
+    class Meta:    
+        model = ApartmentPhoto
+        fields = '__all__'
 
 class AmenitySerializer(ModelSerializer):
-    model = Amenity
-    fields = ['name']
+    class Meta:
+        model = Amenity
+        fields = '__all__' 
 
-class ApartmentListSerializer(ModelSerializer):
-    model = Apartment
+class ApartmentListSerializer(ModelSerializer):    
+    class Meta:
+        model = Apartment 
+        fields = ['title', 'price', 'average_rating', 'cover_photo']
+
+class ApartmentRetriveSerializer(ModelSerializer):
+    photos = ApartmentPhotoSerializer(read_only=True, many=True, source= 'apartmentphotos')
+    
+    class Meta:
+        model = Apartment
+        exclude =  ['created_at', 'updated_at']
+
+class ApartmentCreateUpdateSerializer(ModelSerializer):
+    class Meta:
+        model = Apartment
+        fields = '__all__'
+        read_only_fields = ['average_rating']
+
+class RatingSerializer(ModelSerializer):
+    class Meta:
+        model = Rating
+        fields = '__all__'
+        read_only_fields = ['user']
+
+    def create(self, validated_data):
+        request = self.context["request"]
+        validated_data["user"] = request.user
+        rating, created = Rating.objects.update_or_create(
+            apartment=validated_data["apartment"],
+            user=request.user,
+            defaults={"value": validated_data["value"]},
+        )
+        
+        rating.apartment.update_rating()
+        return rating
+
+
+
+    
+
+
     
